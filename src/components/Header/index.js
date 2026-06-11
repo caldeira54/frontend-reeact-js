62dc60ad550ae0a167b8027652890377c5150252
caldeira54 <caldeiraa54@gmail.com>
2026-06-11 18:20:02 -0300

    Consumo rota de listar cidades

---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import './style.css';
+
+function Header() {
+    return (
+        <header>
+            <h1>Manutenção de Pessoas e Cidades</h1>
+        </header>
+    );
+}
+
+export default Header;
